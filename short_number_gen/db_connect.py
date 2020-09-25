0d27ca7c4ee54087068db103df88716afe42df02
Slzkud <bz73slz@outlook.com>
2020-09-25 16:12:33 +0800

    数据脱敏

--- short_number_gen/db_connect.py
+++ short_number_gen/db_connect.py
@@ -7,14 +7,14 @@ def init_db():
     else:
         c_sl3=sqlite3.connect(config.db_file)
         s_sl3 = c_sl3.cursor()
-        s_sl3.execute('CREATE TABLE "tel" ("ID"  TEXT NOT NULL,"Name"  TEXT NOT NULL,"Type"  TEXT NOT NULL,"DH"  TEXT NOT NULL);')
+        s_sl3.execute('CREATE TABLE "tel" ("Name"  TEXT NOT NULL,"Type"  TEXT NOT NULL,"DH"  TEXT NOT NULL);')
         return c_sl3
 def sqliteEscape(keyWord):
     keyWord = keyWord.replace("'", "''") 
     return keyWord
 def add_item(cur,i):
     sql="INSERT INTO 'tel' VALUES ('{0}', '{1}', '{2}', '{3}');"
-    sql=sql.format(i['WID'],i['XM'],i['BM'],i['DH'])
+    sql=sql.format(i['XM'],i['BM'],i['DH'])
     s_sl3 = cur.cursor()
     s_sl3.execute(sql)
     cur.commit()
